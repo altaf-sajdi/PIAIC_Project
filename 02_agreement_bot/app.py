70285e07263cfa0644fbf69a693a1c82784520b6
altaf sajdi <156938121+altaf-sajdi@users.noreply.github.com>
2024-11-15 19:18:02 +0500

    Add files via upload
    # Ask the user for their favorite animal
    favorite_animal = input("What's your favorite animal? ")
    
    # Respond with the same animal
    print(f"My favorite animal is also {favorite_animal}!")

---
+++ 02_agreement_bot/app.py
@@ -0,0 +1,6 @@
+# 02_agreement_bot
+
+# Ask the user for their favorite animal
+favorite_animal = input("What's your favorite animal? ")
+# Respond with the same animal
+print(f"My favorite animal is also {favorite_animal}!")
