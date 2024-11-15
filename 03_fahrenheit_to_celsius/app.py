c38cb0c34240a15931e5e3b8c086a623b2e74b15
altaf sajdi <156938121+altaf-sajdi@users.noreply.github.com>
2024-11-15 19:24:51 +0500

    Add files via upload
    Problem Statement
    Write a program which prompts the user for a temperature in Fahrenheit (this can be a number with decimal places!) and outputs the temperature converted to Celsius.
    
    The Celsius scale is widely used to measure temperature, but places still use Fahrenheit. Fahrenheit is another unit for temperature, but the scale is different from Celsius -- for example, 0 degrees Celsius is 32 degrees Fahrenheit!
    
    The equation you should use for converting from Fahrenheit to Celsius is the following:
    
    degrees_celsius = (degrees_fahrenheit - 32) * 5.0/9.0
    
    (Note. The .0 after the 5 and 9 matters in the line above!!!)
    
    Here's a sample run of the program (user input is in bold italics):
    
    Enter temperature in Fahrenheit: 76
    
    Temperature: 76.0F = 24.444444444444443C

---
+++ 03_fahrenheit_to_celsius/app.py
@@ -0,0 +1,15 @@
+# 03_fahrenheit_to_celsius
+
+def main():
+    
+    degrees_fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+
+    
+    degrees_celsius = (degrees_fahrenheit - 32) * 5.0 / 9.0
+
+    
+    print(f"Temperature: {degrees_fahrenheit}F = {degrees_celsius}C")
+
+
+if __name__ == '__main__':
+    main()
